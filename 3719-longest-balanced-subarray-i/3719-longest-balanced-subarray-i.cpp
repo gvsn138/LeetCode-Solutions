8216c45b12453a65509ec381689df3e38990582a
Gaurav Suman <86527158+gvsn138@users.noreply.github.com>
2026-02-11 01:49:47 +0530

    Time: 1894 ms (63.56%), Space: 530.6 MB (12.63%) - LeetHub

--- 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
+++ 3719-longest-balanced-subarray-i/3719-longest-balanced-subarray-i.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    inline static uint32_t seen[100001] = {};
-    inline static uint32_t leet = 0;
     int longestBalanced(vector<int>& nums) {
-        leet++;
-        int n = nums.size();
-        int res = 0;
-
-        for (int i = 0; i < n && n - i > res; i++) {
-            int A[2] = {0, 0};
-            uint32_t marker = (leet << 16) | (uint32_t)(i + 1);
-            for (int j = i; j < n; j++) {
-                int val = nums[j];
-                if (seen[val] != marker) {
-                    seen[val] = marker;
-                    A[val & 1]++;
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            unordered_set<int> evenSt;
+            unordered_set<int> oddSt;
+            for (int j = i; j < nums.size(); j++) {
+                if (nums[j] % 2 == 0) {
+                    evenSt.insert(nums[j]);
+                } else {
+                    oddSt.insert(nums[j]);
                 }
-                if (A[0] == A[1])
-                    res = max(res, j - i + 1);
+                if (oddSt.size() == evenSt.size()) {
+                    ans = max(ans, j - i + 1);
+                }
             }
         }
-
-        return res;
+        return ans;
     }
 };
